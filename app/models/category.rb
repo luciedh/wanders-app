ea5fe399b6a5162940c7a217d57f780906931d84
Beata Gyorgy <gybeacska19@gmail.com>
2023-02-04 17:11:23 +0100

    initial seed for places, trips and categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   has_many :place_categories, dependent: :destroy
+  has_many :places, through: :place_categories
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+require "json"
+
+PARIS_API = "https://opendata.paris.fr/api/records/1.0/search/?dataset=paris-autrement-balades-dans-les-arrondissements-peripheriques-poi&q=&rows=157"
+
+poi_json = URI.open(PARIS_API).read
+poi_array = JSON.parse(poi_json)["records"]
+
+poi_array.each do |poi|
+  place = Place.new
+  place.name = poi["fields"]["nom_poi"]
+  address = poi["fields"]["adresse"].nil? ? " " : poi["fields"]["adresse"]
+  city = poi["fields"]["ville"].nil? ? " " : poi["fields"]["ville"]
+  postal_code = poi["fields"]["code_postal"].nil? ? " " : poi["fields"]["code_postal"]
+  place.address = "#{address} , #{postal_code} #{city}"
+  place.geo_lat = poi["fields"]["geo_point_2d"][0]
+  place.geo_long = poi["fields"]["geo_point_2d"][1]
+  place.description = poi["fields"]["texte_description"]
+  nul_url = [nil, "https://capgeo.maps.arcgis.com/sharing/rest/content/items/7a34d77806a44be592fa49079c2ab97c/data"]
+  place.url_image = poi["fields"]["url_image"] unless nul_url.include? poi["fields"]["url_image"]
+  place.save!
+  parcours = poi["fields"]["parcours"].split(";")
+  parcours.each do |p|
+    trip = Trip.where(name: p).first_or_create
+    place.trips << trip
+  end
+  category = Category.where(name: poi["fields"]["categorie"]).first_or_create
+  place.categories << category
+end

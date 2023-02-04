8c3b255d6a6ab1d6c5dd0c65ccd67e88a72c567c
lucie de hauteclocque <lucie.dehauteclocque@gmail.com>
2023-02-04 16:39:26 +0100

    map user home page

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,15 @@
+class PlacesController < ApplicationController
+  def index
+    @places = Place.all
+    @markers = @places.map do |place|
+      {
+        lat: place.geo_lat,
+        lng: place.geo_long
+      }
+    end
+  end
+
+  def show
+
+  end
+end

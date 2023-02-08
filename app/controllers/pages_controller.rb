073aeeab5f157419cdd7e59da445c61fa8cfa3c4
Clement Perrin <113604057+ClemPerr1@users.noreply.github.com>
2023-02-08 13:24:09 +0100

    Make comment design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,6 @@ class PagesController < ApplicationController
   def home
   end
 
-  def ui
+  def custom
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+  get 'comments/create'
   devise_for :users
   root to: "pages#home"
   resources :places, only: [:index, :show]
   resources :trips, only: [:index, :show]
   get "search/trip", to: "trips#search_trip", as: :search_trip
+  get 'custom', to: 'pages#custom'
 end

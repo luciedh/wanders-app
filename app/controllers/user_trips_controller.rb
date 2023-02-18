63a73af915515747f86c048a10a1eb91b4b7c3f8
Beata Gyorgy <gybeacska19@gmail.com>
2023-02-18 15:15:42 +0100

    user_trip model and view

---
+++ app/controllers/user_trips_controller.rb
@@ -0,0 +1,15 @@
+class UserTripsController < ApplicationController
+
+  def create
+    @user_trip = UserTrip.new
+    @user_trip.user = current_user
+    @user_trip.trip = Trip.find(params[:trip_id])
+    if @user_trip.save
+      redirect_to user_trip_path(@user_trip)
+    end
+  end
+
+  def show
+    @user_trip = UserTrip.find(params[:id])
+  end
+end

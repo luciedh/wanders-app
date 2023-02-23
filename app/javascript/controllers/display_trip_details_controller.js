ae5177c028b5df0a757f7bda42ac4ebf4ea297de
lucie de hauteclocque <lucie.dehauteclocque@gmail.com>
2023-02-23 20:57:40 +0100

    trips selector fixed one selection at a time

--- app/javascript/controllers/display_trip_details_controller.js
+++ app/javascript/controllers/display_trip_details_controller.js
@@ -2,7 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="display-trip-details"
 export default class extends Controller {
+  static targets = ["container"]
   revealContent(event) {
+    this.containerTargets.forEach((containerTarget) => {
+      if (containerTarget.classList.contains("active")) {
+        containerTarget.classList.remove("active")
+      }
+    })
     event.currentTarget.classList.toggle("active");
   }
 }
